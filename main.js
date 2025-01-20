1e6b1cf3feba614a25b5e24de24a0ab5e37a807e
val_l <contacttovalerie@gmail.com>
2025-01-19 20:05:51 -0500

    Changed calendar html

--- main.js
+++ main.js
@@ -5,34 +5,3 @@ button.addEventListener('click', function () {
     console.log("copy button clicked");
 });
 
-//test
-
-const button = document.querySelector('h1');
-
-button.addEventListener('click', function () {
-    button.innerText = 'Clicked'; 
-    console.log("copy button clicked");
-});
-
-//new test
-//our stuff
-
-//selects the calendar
-//const getDate = document.getElementById('date');
-//console.log(getDate);
-
-//taking the input and 
-/*getDate.addEventListener('input', function(e) {
-    const clickedDay = new Date (e.target.value);  
-    //console.log(clickedDay);
-
-    //assigned Sunday to index 0, Monday to index 1, Tuesday to index 2, Wednesday to index 3, Thursday to index 4, Friday to index 5, Saturday to index 6
-    let weekDay = Date.getDay();
-    console.log(weekDay);
-
-    
-
-}); */
-
-
-  
