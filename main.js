7d64cb8e90a4115a86227494029ebeec43871e75
NatnaelSisay <natyjava8@gmail.com>
2025-01-29 18:52:50 -0500

    refactor: remove dead code

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 import { dishes } from './assets/arrays.js';
 import { allergies } from './assets/arrays.js';
-import { generateRandomDishes } from "./util.js";
+import {
+    generateRandomDishes,
+    generateWeeklyFood,
+    createWeekdayStructure
+} from "./util.js";
 
-
+//
 let userAlergies = new Set();
 let userPickedStartDate = 0;
-let userPickedEndDate = 7;
 
+const ulAlergies = document.getElementById("alergies");
+
+//
 const calendarStartDate = document.getElementById('calendarStartDatePicker');
-const calendarEndDatePicker = document.getElementById("calendarEndDatePicker");
+const today = new Date();
+const formattedDate = today.toISOString().split("T")[0]; // Format as YYYY-MM-DD
+calendarStartDate.min = formattedDate;
 
 calendarStartDate.addEventListener('input', function (e) {
     //clickedDay is the day someone clicks on
@@ -20,85 +28,8 @@ calendarStartDate.addEventListener('input', function (e) {
     let randomDishes = generateRandomDishes(userAlergies, dishes, 7);
     console.log(randomDishes);
     userPickedStartDate = weekDay;
-    generateWeeklyFood(randomDishes);
+    generateWeeklyFood(randomDishes, userPickedStartDate);
 });
-
-calendarEndDatePicker.addEventListener("input", function (e) {
-    const clickedDay = new Date(e.target.value);
-    userPickedEndDate = clickedDay.getDay();
-    let randomDishes = generateRandomDishes(userAlergies, dishes, 7);
-    console.log(randomDishes);
-    generateWeeklyFood(randomDishes);
-});
-
-
-let ulAlergies = document.getElementById("alergies");
-let daysContainer = document.getElementById("days");
-
-let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-
-function generateWeeklyFood(randomDishes) {
-    daysContainer.innerHTML = "";
-    let numberOfDays = Math.abs(userPickedEndDate - userPickedStartDate);
-
-    for (let startDay = userPickedStartDate; startDay <= (userPickedStartDate + numberOfDays); startDay++) {
-        let index = startDay % 7;
-        daysContainer.appendChild(createWeekdayStructure(days[index], index, randomDishes[index]));
-    }
-
-}
-
-function createWeekdayStructure(day, index, dish) {
-    // Create the parent list item element
-    const li = document.createElement('li');
-    li.className = 'weekday';
-    li.id = 'div' + (index + 1);
-
-    // Create the first child div with Sunday heading
-    const boxDiv = document.createElement('div');
-    boxDiv.className = `box ${day.toLowerCase()}`;
-
-    const h3 = document.createElement('h3');
-    h3.className = 'h3';
-    h3.textContent = day;
-
-    boxDiv.appendChild(h3);
-    li.appendChild(boxDiv);
-
-    // Create the bubble div
-    const bubbleDiv = document.createElement('div');
-    bubbleDiv.className = 'bubble';
-    bubbleDiv.id = day;
-
-    // Create the foodinfo child divs
-    const dishName = document.createElement('div');
-    dishName.className = 'foodinfo';
-    dishName.id = 'dayname';
-    dishName.innerText = dish.name;
-
-
-    const dishIngredients = document.createElement('div');
-    dishIngredients.className = 'foodinfo';
-    dishIngredients.id = 'dayingredients';
-    dishIngredients.innerText = dish.ingredients;
-
-    const dishCalories = document.createElement('div');
-    dishCalories.className = 'foodinfo';
-    dishCalories.id = 'daycalories';
-    dishCalories.innerText = dish.calories;
-
-    // Append the foodinfo divs to the bubble div
-    bubbleDiv.appendChild(dishName);
-    bubbleDiv.appendChild(dishIngredients);
-    bubbleDiv.appendChild(dishCalories);
-
-    // Append the bubble div to the list item
-    li.appendChild(bubbleDiv);
-
-    // Append the entire structure to the document (e.g., to a parent ul)
-    return li;
-}
-
 
 allergies.forEach(allergy => {
     // Create a <li> element
@@ -127,7 +58,6 @@ allergies.forEach(allergy => {
 });
 
 function handleCheckBoxClick(event) {
-    console.log("checkbox clicked", event.target.value);
     let checkBoxValue = event.target.value;
 
     if (userAlergies.has(checkBoxValue)) {
@@ -136,14 +66,6 @@ function handleCheckBoxClick(event) {
         userAlergies.add(checkBoxValue);
     }
 
-    let randomDishes = generateRamdomDishes(userAlergies, dishes, 7);
-    console.log(randomDishes);
-    generateWeeklyFood(randomDishes);
+    let randomDishes = generateRandomDishes(userAlergies, dishes, 7);
+    generateWeeklyFood(randomDishes, userPickedStartDate);
 }
-
-let btn = document.getElementById("randomDishes");
-btn.addEventListener("click", function () {
-    let randomDishes = generateRamdomDishes(userAlergies, dishes, 7);
-    console.log(randomDishes);
-    generateWeeklyFood(randomDishes);
-});
